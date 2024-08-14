afdb702c2838358b92beffe03d37ef6c2ec32263
Максим Ландау <12landay@mail.ru>
2024-08-14 13:31:16 +0300

    f

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MaximLanBowl/Searcher.git/handlers"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server for driver search.
+// @termsOfService https://your-git-repo-url.com
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:5432
+// @BasePath /
+
+
 
 func main() {
-	r := gin.Default()
-	r.GET("/driverSearch", searchDriver)
-	r.Run(":4444")
+    r := gin.Default()
+
+    // Добавляем эндпоинты
+    r.GET("/driverSearch", handlers.SearchDriver)
+
+    // Добавляем Swagger эндпоинты
+    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+    r.Run(":5432")
 }
--- handlers/handler.go
+++ handlers/handler.go
@@ -1,20 +1,29 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"io"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"sync"
+
+	"github.com/MaximLanBowl/Searcher.git/models"
+	"github.com/gin-gonic/gin"
 )
 
-func LoadDrivers(filePath string) ([]Driver, error) {
+func LoadDrivers(filePath string) ([]models.Driver, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var drivers []Driver
+	var drivers []models.Driver
 	r := csv.NewReader(file)
 
 	for {
@@ -28,7 +37,115 @@ func LoadDrivers(filePath string) ([]Driver, error) {
 		driverID, _ := strconv.Atoi(record[0])
 		latitude, _ := strconv.ParseFloat(record[1], 64)
 		longitude, _ := strconv.ParseFloat(record[2], 64)
-		drivers = append(drivers, Driver{driverID})
+		drivers = append(drivers, models.Driver{DriverID: driverID, Latitude: latitude, Longitude: longitude})
 	}
-	
+	return drivers, nil
 }
+
+func GetETA(clientLat, clientLon, driverLat, driverLon float64) (float64, error) {
+	url := "https://maps.starline.ru/api/routing/route"
+	reqBody := map[string]interface{}{
+		"locations": []map[string]interface{}{
+			{"lat": clientLat, "lon": clientLon, "type": "break"},
+			{"lat": driverLat, "lon": driverLon, "type": "break"},
+		},
+		"costing": "auto",
+		"alternates": 1,
+	}
+	jsonBody, _ := json.Marshal(reqBody)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
+
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var routeResponse models.RouteResponse
+    err = json.NewDecoder(resp.Body).Decode(&routeResponse)
+    if err != nil {
+        return 0, err
+    }
+
+	if len(routeResponse.Routes) > 0 {
+		return routeResponse.Routes[0].Duration, nil 
+	}
+	return 0, nil
+}
+
+var drivers []models.Driver
+var once sync.Once
+
+func LoadDriversOnce(){
+	once.Do(func() {
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		filePath := filepath.Join(dir, "driver_positions.csv")
+		drivers, err = LoadDrivers(filePath)
+		if err != nil {
+			log.Fatalf("Ошибка загрузки водителей: %v", err) 
+		}
+	})
+}
+
+func SearchDriver(c *gin.Context) {
+    lat, err := strconv.ParseFloat(c.Query("lat"), 64)
+    if err != nil || lat == 0 {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid latitude"})
+        return
+    }
+    lon, err := strconv.ParseFloat(c.Query("lon"), 64)
+    if err != nil || lon == 0 {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid longitude"})
+        return
+    }
+
+    LoadDriversOnce()
+
+    var closestDriver models.Driver
+    var minETA float64 = -1
+
+    var wg sync.WaitGroup
+    var mu sync.Mutex
+
+    for _, driver := range drivers {
+        wg.Add(1)
+        go func(driver models.Driver) {
+            defer wg.Done()
+            eta, err := GetETA(lat, lon, driver.Latitude, driver.Longitude)
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            mu.Lock()
+            if minETA == -1 || eta < minETA {
+                minETA = eta
+                closestDriver = driver
+            }
+            mu.Unlock()
+        }(driver)
+    }
+    wg.Wait()
+
+    if minETA == -1 {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to find closest driver"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"driver_id": closestDriver.DriverID})
+}
+
+// SearchDriver godoc
+// @Summary Search for the closest driver
+// @Description get closest driver based on client's location
+// @ID search-driver
+// @Accept  json
+// @Produce  json
+// @Param lat query float64 true "Client Latitude"
+// @Param lon query float64 true "Client Longitude"
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /driverSearch [get]
